e23374cfabdcff2dfd7ccefe6bd7e2919414b48e
Henning Günther <h.guenther@tu-bs.de>
2011-02-11 14:56:02 +0100

    Function to convert BDDs into C-expressions

--- lib/Language/GTL/ErrorRefiner.hs
+++ lib/Language/GTL/ErrorRefiner.hs
@@ -2,6 +2,7 @@ module Language.GTL.ErrorRefiner where
 
 import System.Process
 import Data.Maybe (mapMaybe)
+import Data.BDD
 
 parseTrace :: FilePath -> FilePath -> IO [(String,Integer)]
 parseTrace promela trail = do
@@ -10,3 +11,9 @@ parseTrace promela trail = do
                         ["ENTER",proc,st] -> Just (proc,read st)
                         _ -> Nothing
                     ) (lines outp)
+
+generateBDDCheck :: String -> Int -> Tree s Int -> String
+generateBDDCheck name w
+  = foldBDD
+    (\sym l r -> "(("++name++"&(1<<"++show (w-sym)++"))=="++name++")?("++l++"):("++r++")")
+    (\v -> if v then "1" else "0")
