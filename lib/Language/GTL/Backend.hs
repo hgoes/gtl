471dd861d363614470064f14cb6effc3856dc563
Henning Günther <h.guenther@tu-bs.de>
2011-04-08 16:18:36 +0200

    Rough sketch for backend structure

---
+++ lib/Language/GTL/Backend.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE TypeFamilies #-}
+module Language.GTL.Backend where
+
+import Language.GTL.Syntax
+import Data.Map as Map
+import Data.Traversable
+import Prelude hiding (mapM)
+
+class GTLBackend b where
+  type GTLBackendData b
+  backendName :: b -> String
+  initBackend :: b -> [String] -> IO (GTLBackendData b)
+  typeCheckInterface :: b -> GTLBackendData b -> Map String GTLType -> Map String GTLType -> Either String (Map String GTLType,Map String GTLType)
+
+mergeTypes :: Map String GTLType -> Map String GTLType -> Either String (Map String GTLType)
+mergeTypes m1 m2 
+  = mapM id $
+    Map.unionWithKey (\name (Right tp1) (Right tp2) -> if tp1 == tp2
+                                                       then Right tp1
+                                                       else Left $ "Type error for variable "++name++
+                                                            ": gtl-specification says it's "++show tp1++
+                                                            ", but the backend says it's "++show tp2
+                     ) (fmap (Right) m1) (fmap (Right) m2)
---
+++ lib/Language/GTL/Backend/Scade.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE TypeFamilies #-}
+module Language.GTL.Backend.Scade where
+
+import Language.Scade.Lexer (alexScanTokens)
+import Language.Scade.Parser (scade)
+import Language.GTL.Backend
+import qualified Language.Scade.Syntax as Sc
+import Language.GTL.Syntax
+import Data.Map as Map
+
+data Scade = Scade deriving (Show)
+
+instance GTLBackend Scade where
+  type GTLBackendData Scade = (String,[Sc.Declaration])
+  backendName Scade = "scade"
+  initBackend Scade [file,name] = do
+    str <- readFile file
+    return (name,scade $ alexScanTokens str)
+  typeCheckInterface Scade (name,decls) ins outs = do
+    let (sc_ins,sc_outs) = scadeInterface name decls
+    mp_ins <- scadeTypeMap sc_ins
+    mp_outs <- scadeTypeMap sc_outs
+    rins <- mergeTypes ins mp_ins
+    routs <- mergeTypes outs mp_outs
+    return (rins,routs)
+
+scadeTypeToGTL :: Sc.TypeExpr -> Maybe GTLType
+scadeTypeToGTL Sc.TypeInt = Just GTLInt
+scadeTypeToGTL Sc.TypeBool = Just GTLBool
+scadeTypeToGTL _ = Nothing
+
+scadeTypeMap :: [(String,Sc.TypeExpr)] -> Either String (Map String GTLType)
+scadeTypeMap tps = do
+  res <- mapM (\(name,expr) -> case scadeTypeToGTL expr of
+                  Nothing -> Left $ "Couldn't convert SCADE type "++show expr++" to GTL"
+                  Just tp -> Right (name,tp)) tps
+  return $ Map.fromList res
+
+-- | Extract type information from a SCADE model.
+--   Returns two list of variable-type pairs, one for the input variables, one for the outputs.
+scadeInterface :: String -- ^ The name of the Scade model to analyze
+                  -> [Sc.Declaration] -- ^ The parsed source code
+                  -> ([(String,Sc.TypeExpr)],[(String,Sc.TypeExpr)])
+scadeInterface name (op@(Sc.UserOpDecl {}):xs)
+  | Sc.userOpName op == name = (varNames' (Sc.userOpParams op),varNames' (Sc.userOpReturns op))
+  | otherwise = scadeInterface name xs
+    where
+      varNames' :: [Sc.VarDecl] -> [(String,Sc.TypeExpr)]
+      varNames' (x:xs) = (fmap (\var -> (Sc.name var,Sc.varType x)) (Sc.varNames x)) ++ varNames' xs
+      varNames' [] = []
+scadeInterface name (_:xs) = scadeInterface name xs
+scadeInterface name [] = error $ "Couldn't find model "++show name
