ae305a0d7789a6b9796377a2c422005547e72c2c
Henning Günther <h.guenther@tu-bs.de>
2011-04-18 16:18:52 +0200

    New "none" target when no synchronous verification language is available

--- lib/Language/GTL/Backend/All.hs
+++ lib/Language/GTL/Backend/All.hs
@@ -6,6 +6,7 @@ module Language.GTL.Backend.All where
 import Language.GTL.Expression
 import Language.GTL.Backend
 import Language.GTL.Backend.Scade
+import Language.GTL.Backend.None
 import Data.Map as Map
 import Data.Typeable
 
@@ -17,16 +18,27 @@ data AllBackend = AllBackend
                   , allVerifyLocal :: Expr String Bool -> IO (Maybe Bool)
                   }
 
+tryInit :: GTLBackend b => b -> String -> [String] -> IO (Maybe AllBackend)
+tryInit be name args
+  | backendName be == name = do
+    dat <- initBackend be args
+    return $ Just $ AllBackend
+      { allTypecheck = typeCheckInterface be dat
+      , allCInterface = cInterface be dat
+      , allVerifyLocal = backendVerify be dat
+      }
+  | otherwise = return Nothing
+
+firstM :: Monad m => [x -> y -> m (Maybe a)] -> x -> y -> m (Maybe a)
+firstM (x:xs) p q = do
+  r <- x p q
+  case r of
+    Nothing -> firstM xs p q
+    Just rr -> return (Just rr)
+firstM [] _ _ = return Nothing
+
 -- | Try to initialize the correct backend for a given backend name and arguments.
 initAllBackend :: String -- ^ The name of the backend
                   -> [String] -- ^ The arguments with which to initialize the backend
                   -> IO (Maybe AllBackend)
-initAllBackend name args
-  | backendName Scade == name = do
-    dat <- initBackend Scade args
-    return $ Just $ AllBackend 
-      { allTypecheck = typeCheckInterface Scade dat
-      , allCInterface = cInterface Scade dat
-      , allVerifyLocal = backendVerify Scade dat
-      }
-  | otherwise = return Nothing
+initAllBackend = firstM [tryInit Scade,tryInit None]
