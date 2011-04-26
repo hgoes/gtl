4081bc4a799e690fcbed89f57d03f57adcf41a14
Henning Basold <h.basold@tu-bs.de>
2011-04-26 14:11:54 +0200

    rects was just one element, not a list as expected

--- lib/Language/GTL/PrettyPrinter.hs
+++ lib/Language/GTL/PrettyPrinter.hs
@@ -205,11 +205,7 @@ dotToTikz mtp gr
                                   _ -> False) (nodeAttributes nd) of
                     Just (Label (RecordLabel x)) -> x
                     _ -> error "No record label given"
-           rects = case List.find (\attr -> case attr of
-                                  Rects _ -> True
-                                  _ -> False) (nodeAttributes nd) of
-                    Just (Rects x) -> x
-                    _ -> error "No rects given"
+           rects = [x | Rects x <- (nodeAttributes nd)]
            Just reprs = mtp
            (inp,outp,repr,rw,rh) = reprs!(nodeID nd)
            Rect m1 m2 = head (drop (Map.size inp) rects)
