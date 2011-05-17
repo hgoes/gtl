d15bab4d3a3a56d1496b112fb78a06ec0e03c6ca
Henning Günther <h.guenther@tu-bs.de>
2011-05-17 16:28:50 +0200

    Add binary instances for types and values

--- lib/Language/GTL/Types.hs
+++ lib/Language/GTL/Types.hs
@@ -1,6 +1,6 @@
 module Language.GTL.Types where
 
-import Text.Read
+import Text.Read hiding (get)
 import Data.Binary
 import Data.Word
 import Data.List (genericLength)
@@ -14,25 +14,34 @@ data GTLType = GTLInt
              | GTLTuple [GTLType]
              deriving (Eq,Ord)
 
-data GTLValue = GTLIntVal Integer
-              | GTLByteVal Word8
-              | GTLBoolVal Bool
-              | GTLFloatVal Float
-              | GTLEnumVal String
-              | GTLArrayVal [GTLValue]
-              | GTLTupleVal [GTLValue]
-              deriving (Eq,Ord)
+data GTLValue r = GTLIntVal Integer
+                | GTLByteVal Word8
+                | GTLBoolVal Bool
+                | GTLFloatVal Float
+                | GTLEnumVal String
+                | GTLArrayVal [r]
+                | GTLTupleVal [r]
+                deriving (Eq,Ord)
 
-isInstanceOf :: GTLType -> GTLValue -> Bool
-isInstanceOf GTLInt (GTLIntVal _) = True
-isInstanceOf GTLByte (GTLByteVal _) = True
-isInstanceOf GTLBool (GTLBoolVal _) = True
-isInstanceOf GTLFloat (GTLFloatVal _) = True
-isInstanceOf (GTLEnum enums) (GTLEnumVal x) = elem x enums
-isInstanceOf (GTLArray sz tp) (GTLArrayVal els) = (and (fmap (isInstanceOf tp) els)) && (sz == genericLength els)
-isInstanceOf (GTLTuple []) (GTLTupleVal []) = True
-isInstanceOf (GTLTuple (tp:tps)) (GTLTupleVal (v:vs)) = (isInstanceOf tp v) && (isInstanceOf (GTLTuple tps) (GTLTupleVal vs))
-isInstanceOf _ _ = False
+instance Functor GTLValue where
+  fmap _ (GTLIntVal i) = GTLIntVal i
+  fmap _ (GTLByteVal i) = GTLByteVal i
+  fmap _ (GTLBoolVal i) = GTLBoolVal i
+  fmap _ (GTLFloatVal i) = GTLFloatVal i
+  fmap _ (GTLEnumVal i) = GTLEnumVal i
+  fmap f (GTLArrayVal i) = GTLArrayVal (fmap f i)
+  fmap f (GTLTupleVal i) = GTLTupleVal (fmap f i)
+
+isInstanceOf :: GTLType -> (r -> GTLType) -> GTLValue r -> Bool
+isInstanceOf GTLInt _ (GTLIntVal _) = True
+isInstanceOf GTLByte _ (GTLByteVal _) = True
+isInstanceOf GTLBool _ (GTLBoolVal _) = True
+isInstanceOf GTLFloat _ (GTLFloatVal _) = True
+isInstanceOf (GTLEnum enums) _ (GTLEnumVal x) = elem x enums
+isInstanceOf (GTLArray sz tp) f (GTLArrayVal els) = (and (fmap (tp==) (fmap f els))) && (sz == genericLength els)
+isInstanceOf (GTLTuple []) _ (GTLTupleVal []) = True
+isInstanceOf (GTLTuple (tp:tps)) f (GTLTupleVal (v:vs)) = (tp==(f v)) && (isInstanceOf (GTLTuple tps) f (GTLTupleVal vs))
+isInstanceOf _ _ _ = False
 
 intersperseS :: ShowS -> [ShowS] -> ShowS
 intersperseS i [] = id
@@ -56,18 +65,21 @@ instance Show GTLType where
                                intersperseS (showString ", ") (fmap (showsPrec 0) tps) .
                                showChar ')'
 
-instance Show GTLValue where
-  showsPrec p (GTLIntVal v) = showsPrec p v
-  showsPrec p (GTLByteVal v) = showsPrec p v
-  showsPrec p (GTLBoolVal v) = showsPrec p v
-  showsPrec p (GTLFloatVal v) = showsPrec p v
-  showsPrec p (GTLEnumVal v) = showString v
-  showsPrec p (GTLArrayVal vals) = showChar '(' .
-                                   intersperseS (showString ", ") (fmap (showsPrec 0) vals) .
-                                   showChar ')'
-  showsPrec p (GTLTupleVal vals) = showChar '(' .
-                                   intersperseS (showString ", ") (fmap (showsPrec 0) vals) .
-                                   showChar ')'
+showGTLValue :: (r -> String) -> Int -> GTLValue r -> ShowS
+showGTLValue _ p (GTLIntVal v) = showsPrec p v
+showGTLValue _ p (GTLByteVal v) = showsPrec p v
+showGTLValue _ p (GTLBoolVal v) = showsPrec p v
+showGTLValue _ p (GTLFloatVal v) = showsPrec p v
+showGTLValue _ p (GTLEnumVal v) = showString v
+showGTLValue f p (GTLArrayVal vals) = showChar '(' .
+                                      intersperseS (showString ", ") (fmap (showString.f) vals) .
+                                      showChar ')'
+showGTLValue f p (GTLTupleVal vals) = showChar '(' .
+                                      intersperseS (showString ", ") (fmap (showString.f) vals) .
+                                      showChar ')'
+
+instance Show r => Show (GTLValue r) where
+  showsPrec = showGTLValue show
 
 readIntersperse :: ReadPrec b -> ReadPrec a -> ReadPrec [a]
 readIntersperse i f = (do
@@ -135,5 +147,46 @@ instance Read GTLType where
           _ -> pfail
 
 instance Binary GTLType where
-  put = undefined
-  get = undefined
+  put GTLInt = put (0::Word8)
+  put GTLByte = put (1::Word8)
+  put GTLBool = put (2::Word8)
+  put GTLFloat = put (3::Word8)
+  put (GTLEnum xs) = put (4::Word8) >> put xs
+  put (GTLArray sz tp) = put (5::Word8) >> put sz >> put tp
+  put (GTLTuple tps) = put (6::Word8) >> put tps
+  get = do
+    i <- get
+    case (i::Word8) of
+      0 -> return GTLInt
+      1 -> return GTLByte
+      2 -> return GTLBool
+      3 -> return GTLFloat
+      4 -> do
+        xs <- get
+        return (GTLEnum xs)
+      5 -> do
+        sz <- get
+        tp <- get
+        return (GTLArray sz tp)
+      6 -> do
+        tps <- get
+        return (GTLTuple tps)
+
+instance Binary r => Binary (GTLValue r) where
+  put (GTLIntVal x) = put (0::Word8) >> put x
+  put (GTLByteVal x) = put (1::Word8) >> put x
+  put (GTLBoolVal x) = put (2::Word8) >> put x
+  put (GTLFloatVal x) = put (3::Word8) >> put x
+  put (GTLEnumVal x) = put (4::Word8) >> put x
+  put (GTLArrayVal x) = put (5::Word8) >> put x
+  put (GTLTupleVal x) = put (6::Word8) >> put x
+  get = do
+    i <- get
+    case (i::Word8) of
+      0 -> fmap GTLIntVal get
+      1 -> fmap GTLByteVal get
+      2 -> fmap GTLBoolVal get
+      3 -> fmap GTLFloatVal get
+      4 -> fmap GTLEnumVal get
+      5 -> fmap GTLArrayVal get
+      6 -> fmap GTLTupleVal get
