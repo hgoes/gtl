e0bafd37cd43c4619f1e90571fe4f799dbf2fcfd
Henning Basold <h.basold@tu-bs.de>
2011-08-12 11:50:06 +0200

    Properly name in english terms
    Hopefully no one thinks we have anything to do with drugs or so...

--- lib/Language/GTL/DFA.hs
+++ lib/Language/GTL/DFA.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
 
-module Language.GTL.DEA (
-    DEA(..), determinizeBA, minimizeDEA
+module Language.GTL.DFA (
+    DFA(..), determinizeBA, minimizeDFA
   )
   where
 
@@ -24,24 +24,24 @@ data MakeTotal m = MakeTotal { unTotal :: m }
 instance Ord a => TotalFunction (MakeTotal (Map a b)) a b where
   m !$ k = fromJust $ Map.lookup k $ unTotal m
 
-type DEATransitionFunc a st = MakeTotal (Map st (Map a st))
+type DFATransitionFunc a st = MakeTotal (Map st (Map a st))
 
-{-| Not a real DEA. Has no final states as they are not needed (the acceptance condition
+{-| Not a real DFA. Has no final states as they are not needed (the acceptance condition
     of the Buchi automaton can not be represented). Also the transition function is only
     partial. The semantics are that there is a virtual failure state into which an
     executing algorithm will go if no transition can be taken. -}
-data DEA a st = DEA { deaTransitions :: DEATransitionFunc a st
-                  , deaInit :: st
-                  -- , deaFinals :: Set st -- Not needed at the moment
+data DFA a st = DFA { dfaTransitions :: DFATransitionFunc a st
+                  , dfaInit :: st
+                  -- , dfaFinals :: Set st -- Not needed at the moment
                   }
 
-instance (Show a,Show st,Ord st) => Show (DEA a st) where
-  show dea = unlines $ concat [ [(if st == (deaInit dea)
+instance (Show a,Show st,Ord st) => Show (DFA a st) where
+  show dfa = unlines $ concat [ [(if st == (dfaInit dfa)
                                  then "initial "
                                  else "") ++
                                 "state "++show st]++
                                [ "  "++show cond++" -> "++show trg | (cond,trg) <- Map.toList trans ]
-                             | (st,trans) <- Map.toList $ unTotal $ deaTransitions dea ]
+                             | (st,trans) <- Map.toList $ unTotal $ dfaTransitions dfa ]
 
 -- Make clear that the transition function of the Buchi is expected to be total.
 type BATransitionFunc a st = MakeTotal (Map st (Set (a, st)))
@@ -62,16 +62,16 @@ mergeInits ba =
   }
 
 -- | Tries to determinize a given B&#xFC;chi automaton. Only possible if all states are final.
-determinizeBA :: (Eq a, Ord a, Eq st, Ord st, BAState st) => BA a st -> DEA a (PowSetSt st)
+determinizeBA :: (Eq a, Ord a, Eq st, Ord st, BAState st) => BA a st -> DFA a (PowSetSt st)
 determinizeBA ba
-  | (Set.size $ baFinals ba) /= (Map.size $ baTransitions ba) = error "Not able to transform BA into DEA."
+  | (Set.size $ baFinals ba) /= (Map.size $ baTransitions ba) = error "Not able to transform BA into DFA."
   | otherwise =
       let ba' = mergeInits ba
           initS = Set.singleton $ Set.findMin $ baInits ba
           (trans, states) = determinize' (MakeTotal $ baTransitions ba) (Set.singleton initS) (Map.empty, Set.empty)
-      in DEA {
-          deaTransitions = MakeTotal trans,
-          deaInit = initS
+      in DFA {
+          dfaTransitions = MakeTotal trans,
+          dfaInit = initS
         }
       where
         determinize' :: (Eq a, Ord a, Eq st, Ord st) =>
@@ -112,43 +112,43 @@ determinizeBA ba
         targetStates :: (Eq st, Ord st) => Map a (PowSetSt st) -> Set (PowSetSt st)
         targetStates = foldl (flip Set.insert) Set.empty
 
-minimizeDEA :: (Eq a, Ord a, Eq st, Ord st, Show a, Show st) => DEA a st -> DEA a (PowSetSt st)
-minimizeDEA dea =
-  let initEqClasses = Set.fromList [states dea] -- if there would be any final states: `Set.difference` (deaFinals dea), deaFinals dea]
-      eqClasses = findEqClasses dea initEqClasses
-      trans' = buildMinDEA dea eqClasses
-      init' = fromJust $ find (Set.member $ deaInit dea) eqClasses
-  in DEA (MakeTotal trans') init'
+minimizeDFA :: (Eq a, Ord a, Eq st, Ord st, Show a, Show st) => DFA a st -> DFA a (PowSetSt st)
+minimizeDFA dfa =
+  let initEqClasses = Set.fromList [states dfa] -- if there would be any final states: `Set.difference` (dfaFinals dfa), dfaFinals dfa]
+      eqClasses = findEqClasses dfa initEqClasses
+      trans' = buildMinDFA dfa eqClasses
+      init' = fromJust $ find (Set.member $ dfaInit dfa) eqClasses
+  in DFA (MakeTotal trans') init'
   where
-    states = (foldl (foldl $ flip Set.insert) Set.empty) . unTotal . deaTransitions
+    states = (foldl (foldl $ flip Set.insert) Set.empty) . unTotal . dfaTransitions
 
     getEquivClass eqClasses s = fromJust $ find (Set.member s) eqClasses -- the state is guaranteed to be in some class
 
-    findEqClasses :: (Eq a, Ord a, Eq st, Ord st, Show a, Show st) => DEA a st -> Set (PowSetSt st) -> Set (PowSetSt st)
-    findEqClasses dea eqClasses =
-      let eqClasses' = foldl (\eqs' e -> eqs' `Set.union` (splitClass dea eqClasses e)) Set.empty eqClasses
-      in if eqClasses /= eqClasses' then findEqClasses dea eqClasses' else eqClasses'
+    findEqClasses :: (Eq a, Ord a, Eq st, Ord st, Show a, Show st) => DFA a st -> Set (PowSetSt st) -> Set (PowSetSt st)
+    findEqClasses dfa eqClasses =
+      let eqClasses' = foldl (\eqs' e -> eqs' `Set.union` (splitClass dfa eqClasses e)) Set.empty eqClasses
+      in if eqClasses /= eqClasses' then findEqClasses dfa eqClasses' else eqClasses'
 
-    splitClass :: (Eq a, Ord a, Eq st, Ord st, Show a, Show st) => DEA a st -> Set (Set st) -> Set st -> Set (Set st)
-    splitClass dea eqClasses eqClass =
+    splitClass :: (Eq a, Ord a, Eq st, Ord st, Show a, Show st) => DFA a st -> Set (Set st) -> Set st -> Set (Set st)
+    splitClass dfa eqClasses eqClass =
       let (next:rest) = Set.toList eqClass -- equivalence classes are never empty
-      in splitClass' dea eqClasses (Set.singleton $ Set.singleton next) rest
+      in splitClass' dfa eqClasses (Set.singleton $ Set.singleton next) rest
       where
-        splitClass' dea eqClasses eqClasses' eqClass = foldl (putIntoClass dea eqClasses) eqClasses' eqClass
-        putIntoClass dea eqClasses eqClasses' s =
-          let (eqClasses'', inserted) = foldl (\(cs,f) c -> if transMatch dea eqClasses (Set.findMin c) s then (Set.insert (Set.insert s c) cs, True) else (Set.insert c cs, f)) (Set.empty, False) eqClasses'
+        splitClass' dfa eqClasses eqClasses' eqClass = foldl (putIntoClass dfa eqClasses) eqClasses' eqClass
+        putIntoClass dfa eqClasses eqClasses' s =
+          let (eqClasses'', inserted) = foldl (\(cs,f) c -> if transMatch dfa eqClasses (Set.findMin c) s then (Set.insert (Set.insert s c) cs, True) else (Set.insert c cs, f)) (Set.empty, False) eqClasses'
           in if not inserted then Set.insert (Set.singleton s) eqClasses'' else eqClasses''
-        transMatch dea eqClasses s1 s2 =
-          let t1 = (deaTransitions dea) !$ s1
-              t2 = (deaTransitions dea) !$ s2
+        transMatch dfa eqClasses s1 s2 =
+          let t1 = (dfaTransitions dfa) !$ s1
+              t2 = (dfaTransitions dfa) !$ s2
               d1 = Map.differenceWith (equiv eqClasses) t1 t2
               d2 = Map.differenceWith (equiv eqClasses) t2 t1
           in Map.null d1 && Map.null d2 -- symmetric difference
         equiv eqClasses s1 s2 = if Set.member s2 $ getEquivClass eqClasses s1 then Nothing else Just s1
 
-    buildMinDEA :: (Eq a, Ord a, Eq st, Ord st) => DEA a st -> Set (PowSetSt st) -> Map (PowSetSt st) (Map a (PowSetSt st))
-    buildMinDEA dea eqClasses = foldl (\trans s -> Map.insert s (buildTrans dea eqClasses s) trans) Map.empty eqClasses
+    buildMinDFA :: (Eq a, Ord a, Eq st, Ord st) => DFA a st -> Set (PowSetSt st) -> Map (PowSetSt st) (Map a (PowSetSt st))
+    buildMinDFA dfa eqClasses = foldl (\trans s -> Map.insert s (buildTrans dfa eqClasses s) trans) Map.empty eqClasses
       where
-        -- Takes a state in the minimized DEA and builds the transitions originating there.
-        buildTrans :: (Eq a, Ord a, Eq st, Ord st) => DEA a st -> Set (PowSetSt st) -> PowSetSt st -> Map a (PowSetSt st)
-        buildTrans dea eqClasses = foldl (\trans s -> trans `Map.union` (Map.map (getEquivClass eqClasses) (deaTransitions dea !$ s))) Map.empty
+        -- Takes a state in the minimized DFA and builds the transitions originating there.
+        buildTrans :: (Eq a, Ord a, Eq st, Ord st) => DFA a st -> Set (PowSetSt st) -> PowSetSt st -> Map a (PowSetSt st)
+        buildTrans dfa eqClasses = foldl (\trans s -> trans `Map.union` (Map.map (getEquivClass eqClasses) (dfaTransitions dfa !$ s))) Map.empty
