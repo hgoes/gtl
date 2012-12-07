8c9aa1304867fcedc8aa94907c409a0d7d2c34dc
Daniel Hummel <y0060619@tu-bs.de>
2012-12-07 17:26:46 +0100

    build LTL generate process parameter

--- lib/Language/GTL/Target/NuSMV.hs
+++ lib/Language/GTL/Target/NuSMV.hs
@@ -8,21 +8,14 @@ module Language.GTL.Target.NuSMV
        (verifyModel) where
 
 import Language.GTL.Model
+import Language.GTL.LTL as LTL
+import Language.GTL.Expression
 import Language.NuSMV.Syntax as S
 import Language.NuSMV.Pretty as S
-import Language.GTL.Target.Common
 import Language.GTL.Translation
 
-import Data.Set as Set hiding (foldl)
 import Data.Map as Map hiding (foldl)
-import Data.List (elemIndex,genericLength,genericIndex)
-import Data.Foldable
 import Prelude hiding (foldl,concat,foldl1,mapM)
-import Data.Maybe
-import Data.Int
-
-import Misc.ProgramOptions as Opts
-import Misc.VerificationEnvironment
 
 -- | Do a comp lete verification of a given GTL file
 verifyModel :: --Opts.Options -- ^ Options
@@ -33,7 +26,7 @@ verifyModel spec = do
   let modules = translateSpec spec
   --traceFiles <- runVerification opts name pr
   --parseTraces opts name traceFiles (traceToAtoms model)
-  putStrLn $ show ltlSpec
+  --putStrLn $ show ltlSpec
   putStrLn $ show $ prettyProgram modules
 
 translateSpec :: GTLSpec String -> [S.Module]
@@ -53,14 +46,15 @@ translateSpec spec = [
  [buildMainModule spec]
 
 translateModel :: GTLModel a -> [ModuleElement]
-translateModel m = VarDeclaration [
-  (outp, ???)
+translateModel m = []--VarDeclaration [
+{--  (outp
+   , SimpleType $ TypeWord {typeWordSigned = False, typeWordBits = ConstExpr $ ConstId "?"}
 | (outp, tp) <- Map.toList $ gtlModelOutput m
 ]++
 [
  InitConstraint (BasicExpr inp v)
 | (inp, v) <- Map.toList $ gtlModelDefaults m
-]
+]--}
 
 buildMainModule :: GTLSpec String -> S.Module
 buildMainModule spec = S.Module {
@@ -72,20 +66,32 @@ buildMainModule spec = S.Module {
  , let mname = gtlInstanceModel inst
        m = (gtlSpecModels spec)!mname
        conn = gtlSpecConnections spec
- ]]++[LTLSpec $ gtlToLTL Nothing (gtlSpecVerify spec)]
+ ]]++[LTLSpec $ buildLTLSpec $ gtlToLTL Nothing (gtlSpecVerify spec)]
 }
 
+buildLTLSpec :: (Show a) => LTL.LTL (TypedExpr a) -> BasicExpr
+buildLTLSpec (Atom e) = ConstExpr $ ConstId $ show e
+buildLTLSpec (Bin op (e1) (e2)) = case op of
+  LTL.And -> BinExpr OpAnd (buildLTLSpec e1) (buildLTLSpec e2)
+  LTL.Or -> BinExpr OpOr (buildLTLSpec e1) (buildLTLSpec e2)
+  LTL.Until -> BinExpr LTLU (buildLTLSpec e1) (buildLTLSpec e2)
+  LTL.UntilOp -> BinExpr LTLV (buildLTLSpec e1) (buildLTLSpec e2)
+buildLTLSpec (Un op (e)) = case op of
+  LTL.Not -> UnExpr OpNot (buildLTLSpec e)
+buildLTLSpec (Ground b) = ConstExpr $ ConstBool b
+
 buildProcessParam :: String 
-                     -> [(GTLConnectionPoint a,GTLConnectionPoint a)] 
+                     -> [(GTLConnectionPoint String,GTLConnectionPoint String)] 
                      -> [BasicExpr] 
-buildProcessParam n (((GTLConnPt f fv fi), _):xs) = ( -- nur links weil p.x -> c.ix also p schreibt in ix aber nicht ix in p.x somit nur left betrachten
- if f == n then 
-   IdExpr ComplexId {
-     idBase = Just f
-     , idNavigation = [Left "fv"] -- TODO fv into String
-   }
- else
-   []
+{--buildProcessParam n (((GTLConnPt f fv fi), _):xs) = ( -- nur links weil p.x -> c.ix also p schreibt in ix aber nicht ix in p.x somit nur left betrachten
  ):(buildProcessParam n xs)
-buildProcessParam n [] = []
+buildProcessParam n [] = []--}
+buildProcessParam n conns = [
+  IdExpr ComplexId {
+    idBase = Just f
+    , idNavigation = [Left fv] -- TODO fv into String
+  }
+ | ((GTLConnPt f fv fi), (GTLConnPt t _ _)) <- conns
+ , n == t
+ ]
 
